07dce7e570b31643c198542f6f4caf3ccceb3aa4
ivaaanrm <iromero.py@gmail.com>
2022-10-03 12:30:53 +0200

    preuba funciones

--- P1/pav_analysis.c
+++ P1/pav_analysis.c
@@ -1,14 +1,40 @@
 #include <math.h>
 #include "pav_analysis.h"
+#include <stdio.h>
 
 float compute_power(const float *x, unsigned int N) {
-    return 0;
+    int i;
+    double sum = 0.0;
+    for(i = 0; i < N; i++){
+        sum += x[i]*x[i];
+    }
+    return 10*log10(sum/N);
 }
 
 float compute_am(const float *x, unsigned int N) {
-    return 0;
+    int i;
+    double sum = 0.0;
+    for(i = 0; i < N; i++){
+        sum += fabs(x[i]);
+    }
+    // printf("%f\n", sum);
+    return sum/N;
 }
 
 float compute_zcr(const float *x, unsigned int N, float fm) {
-    return 0;
+    int i;
+    double sum = 0.0;
+    for (i = 0; i < N; i++){
+        if (sgn(x[i]) != sgn(x[i-1])){
+            sum += 1;
+        }
+    }
+
+    return fm/(2*(N-1))*sum;
+}
+
+int sgn(double x) {
+  if (x > 0.0) return 1;
+  if (x < 0.0) return -1;
+  return 0;
 }
--- P1/pav_analysis.h
+++ P1/pav_analysis.h
@@ -1,6 +1,7 @@
 #ifndef PAV_ANALYSIS_H
 #define PAV_ANALYSIS_H
 
+int sgn(double x);
 float compute_power(const float *x, unsigned int N);
 float compute_am(const float *x, unsigned int N);
 float compute_zcr(const float *x, unsigned int N, float fm);
